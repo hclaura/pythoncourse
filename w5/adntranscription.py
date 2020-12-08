4a0657dcde9e170065ec94c2c5ae39f77a7db173
hclaura <hclaurax@gmail.com>
2020-12-08 07:17:55 +0000

    w5

---
+++ w5/adntranscription.py
@@ -0,0 +1,117 @@
+nucleotides = ["A", "C", "T", "G"]
+
+nucl_complement = {
+               "A":"T", 
+               "C":"G", 
+               "T":"A",
+               "G":"C" 
+	      }
+
+rna_transcription = {
+	       "A":"U",
+	       "C":"G",
+               "T":"A",
+               "G":"C"
+	      }
+odons = {
+  "UUU": "Fenilalanina",
+  "UUC": "Fenilalanina",
+  "UUA": "Leucina",
+  "UUG": "Leucina",
+  "UCU": "Serina",
+  "UCC": "Serina",
+  "UCA": "Serina",
+  "UCG": "Serina",
+  "UAU": "Tirosina",
+  "UAC": "Tirosina",
+  "UAA": "Codon de terminacion ocre",
+  "UAG": "Codon de terminacin Ambar",
+  "UGU": "Cisteina",
+  "UGC": "Cisteina",
+  "UGA": "Codon de terminacion Opalo",
+  "UGG": "Triptofano",
+  "CUU": "Leucina",
+  "CUC": "Leucina",
+  "CUA": "Leucina",
+  "CUG": "Leucina",
+  "CCU": "Prolina",
+  "CCC": "Prolina",
+  "CCA": "Prolina",
+  "CCG": "Prolina",
+  "CAU": "Histidina",
+  "CAC": "Histidina",
+  "CAA": "Glutamina",
+  "CAG": "Glutamina",
+  "CGU": "Arginina",
+  "CGC": "Arginina",
+  "CGA": "Arginina",
+  "CGG": "Arginina",
+  "AUU": "Isoleucina",
+  "AUC": "Isoleucina",
+  "AUA": "Isoleucina",
+  "AUG": "Metionina",
+  "ACU": "Treonina",
+  "ACC": "Treonina",
+  "ACA": "Treonina",
+  "ACG": "Treonina",
+  "AAU": "Asparagina",
+  "AAC": "Asparagina",
+  "AAA": "Lisina",
+  "AAG": "Lisina",
+  "AGU": "Serina",
+  "AGC": "Serina",
+  "AGA": "Arginina",
+  "AGG": "Arginina",
+  "GUU": "Valina",
+  "GUC": "Valina",
+  "GUA": "Valina",
+  "GUG": "Valina",
+  "GCU": "Alanina",
+  "GCC": "Alanina",
+  "GCA": "Alanina",
+  "GCG": "Alanina",
+  "GAU": "acido asartico",
+  "GAC": "acido asartico",
+  "GAA": "acido glutamico",
+  "GAG": "acido glutamico",
+  "GGU":"Glicina",
+  "GGC": "Glicina",
+  "GGA": "Glicina",
+  "GGG": "Glicina"
+}
+
+nucl_codon = [];
+
+dna = ATGTACTCATCGTTTCGGAAGAGAGGTACGTTAATAGTTAAATAGCGTACTTCTTTTTCTTGCTTTCGTGGTATTCTTGCTAGTTAC
+dna_complement = ""
+rna = ""
+for x in dna:
+  dna_complement = dna_complement + nucl_complement[x]
+  
+
+n = len(dna)
+print " dna length  = ", n
+
+n = len(dna_complement)
+print " dna length complementary = ", n 
+
+for i in range(0, n):
+  print i, dna[i], dna_complement[i]
+
+print "dna complement : "
+print dna_complement
+
+for x in dna: 
+  rna = rna + rna_transcription[x]
+
+print "rna "
+print rna[::-1]
+
+print rna
+n = len(rna)
+
+for i in range(0, n, 3):
+ nucl_codon.append(rna[i:i+3])
+
+n = len(nucl_codon)
+print "total codons ", n
---
+++ w5/files.py
@@ -0,0 +1,15 @@
+# files
+# adn example
+adn = ""
+
+f = open("covid19.txt", "r")
+for x in f:
+  adn = adn + x
+
+adn = adn.replace('\n','')
+
+n = len(adn)
+print " adn length = ", n
+print adn
+for i in range(0, n):
+  print i+1,  adn[i]
